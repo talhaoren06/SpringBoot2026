03903d2148a2244a5abe91be7d61530a51e30f93
Ahmet Talha Oren <talhaoren27@gmail.com>
2026-09-24 20:27:23 +0300

    Initial commit

---
+++ src/main/java/com/talhaoren/springboot2026/SpringBoot2026Application.java
@@ -0,0 +1,13 @@
+package com.talhaoren.springboot2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/talhaoren/springboot2026/SpringBoot2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talhaoren.springboot2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
